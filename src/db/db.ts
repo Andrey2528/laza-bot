c70085d820daf7e03e14ffeb7acd4c455fe6e894
Andrey2528 <55595617+Andrey2528@users.noreply.github.com>
2023-04-11 10:13:47 +0300

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,10 @@
 import { DataTypes, Sequelize } from 'sequelize'
 
-/*const sequelize = new Sequelize('laza-db', 'postgres', 'VIsFCxAF7jucKIJ', { //название дб, имя пользователя и пароль(паросль оставил пустным)
+const sequelize = new Sequelize('laza-bot-db', 'postgres', 'VIsFCxAF7jucKIJ', {
+  host: 'laza-bot-db.flycast',
   dialect: 'postgres',
-  //host: 'laza-db.internal',
-  host: process.env.PSQL_HOST || "laza-bot-db.flycast:",
-  port: 8080,
-})*/
-
-const sequelize = new Sequelize('postgres://postgres:VIsFCxAF7jucKIJ@laza-bot-db.flycast:5432')
-
+  port: 8080
+})
 
 const Users = sequelize.define('Users', {
   userId: { type: DataTypes.INTEGER },
