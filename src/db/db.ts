6e0dfd4d25f833c7d394e84a917645b7d19c8e6e
Andrey2528 <andreys20.2016@ukr.net>
2023-04-11 09:13:46 +0300

    update database conection, index.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import { DataTypes, Sequelize } from 'sequelize'
 const sequelize = new Sequelize('laza-db', 'postgres', 'M6KlebgYJt7jnfK', { //название дб, имя пользователя и пароль(паросль оставил пустным)
   dialect: 'postgres',
   //host: 'laza-db.internal',
-  host: process.env.PSQL_HOST || "laza-db.fly.dev",
+  host: process.env.PSQL_HOST || "fdaa:1:e2a4:0:1::2",
   port: 5432,
 
 })
